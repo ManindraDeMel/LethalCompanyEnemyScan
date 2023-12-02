988167a46125c466aeb9fa602431a57d0fbffe59
Manindra de Mel <u7156805@anu.edu.au>
2023-12-03 03:15:44 +1100

    Add project files.

---
+++ EnemyScan/Patches/EnemyScan.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using TerminalApi;
+using EnemyScan;
+
+namespace EnemyScan.Helper
+{
+    public static class ScanEnemies
+    {
+        public static string enemyString = "No Enemies Found.";
+
+        [HarmonyPatch(typeof(RoundManager), "RefreshEnemiesList()")]
+        [HarmonyPostfix]
+        public static void GetEnemies(List<EnemyAI> ___spawnedAIs)
+        {
+            enemyString = BuildEnemyCountString(___spawnedAIs);
+        }
+        private static string BuildEnemyCountString(List<EnemyAI> enemies)
+        {
+            var enemyCount = new Dictionary<string, int>();
+
+            // Counting occurrences
+            foreach (var enemy in enemies)
+            {
+                string enemyName = enemy.enemyType.enemyName;
+
+                if (enemyCount.ContainsKey(enemyName))
+                {
+                    enemyCount[enemyName]++;
+                }
+                else
+                {
+                    enemyCount[enemyName] = 1;
+                }
+            }
+
+            // Building the string
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in enemyCount)
+            {
+                sb.AppendLine($"{pair.Key}: {pair.Value}");
+            }
+
+            return sb.ToString();
+        }
+    }
+    [HarmonyPatch(typeof(StartOfRound))]
+    public static class MoonManager
+    {
+        public static bool isOnMoon = true;
+
+        [HarmonyPatch("StartGame")]
+        [HarmonyPostfix]
+        public static void StartingGame()
+        {
+            isOnMoon = true;
+            EnemyScan.UpdateEnemyCommand();
+        }
+
+        [HarmonyPatch("ShipHasLeft")]
+        [HarmonyPostfix]
+        public static void EndingGame()
+        {
+            isOnMoon = false;
+            EnemyScan.UpdateEnemyCommand();
+        }
+
+    }
+}
---
+++ EnemyScan/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using HarmonyLib;
+using TerminalApi;
+using static TerminalApi.TerminalApi;
+using EnemyScan.Helper;
+using System.Reflection;
+namespace EnemyScan
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("atomic.terminalapi")]
+    public class EnemyScan : BaseUnityPlugin
+    {
+        private const string modGUID = "299792458.EnemyScan";
+        private const string modName = "EnemyScan";
+        private const string modVersion = "1.0.0";
+
+        void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            
+            TerminalKeyword verbKeyword = CreateTerminalKeyword("list", true);
+            TerminalKeyword nounKeyword = CreateTerminalKeyword("enemies");
+            TerminalNode triggerNode = CreateTerminalNode($"ERROR! No Enemies in space\n", true);
+
+            verbKeyword = verbKeyword.AddCompatibleNoun(nounKeyword, triggerNode);
+            nounKeyword.defaultVerb = verbKeyword;
+
+            AddTerminalKeyword(verbKeyword);
+            AddTerminalKeyword(nounKeyword);
+        }
+
+        public static void UpdateEnemyCommand()
+        {
+            if (MoonManager.isOnMoon)
+            {
+                UpdateKeywordCompatibleNoun("list", "enemies", CreateTerminalNode($"{ScanEnemies.enemyString}.\n", true));
+            }
+            else
+            {
+                UpdateKeywordCompatibleNoun("list", "enemies", CreateTerminalNode($"ERROR! No Enemies in space\n", true));
+            }
+        }
+    }
+}
