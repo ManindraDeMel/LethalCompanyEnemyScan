7fedde87a1e5fa72afd799c619b98c5a9c4299db
kohanis <kohanis@ya.ru>
2023-12-24 12:30:08 +0300

    Move to TerminalParsedSentence event

---
+++ EnemyScan/EnemyScan.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Text;
+using BepInEx;
+using BepInEx.Logging;
+using TerminalApi;
+using TerminalApi.Events;
+using static TerminalApi.TerminalApi;
+
+namespace EnemyScan;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("atomic.terminalapi")]
+public class EnemyScan : BaseUnityPlugin
+{
+    private const string DefaultString = "No Enemies Found.\n\n";
+    private static TerminalNode _triggerNode = null!;
+    private static ManualLogSource _log = null!;
+
+    private void Awake()
+    {
+        _log = Logger;
+
+        var verbKeyword = CreateTerminalKeyword("list", true);
+        var nounKeyword = CreateTerminalKeyword("enemies");
+        nounKeyword.defaultVerb = verbKeyword;
+
+        _triggerNode = CreateTerminalNode(DefaultString, true);
+        verbKeyword.AddCompatibleNoun(nounKeyword, _triggerNode);
+
+        AddTerminalKeyword(verbKeyword);
+        AddTerminalKeyword(nounKeyword);
+
+        Events.TerminalParsedSentence += OnTerminalParsedSentence;
+    }
+
+    private static void OnTerminalParsedSentence(object sender, Events.TerminalParseSentenceEventArgs e)
+    {
+        if (e.ReturnedNode != _triggerNode)
+            return;
+
+        _triggerNode.displayText = BuildEnemyCountString();
+    }
+
+    private static string BuildEnemyCountString()
+    {
+        var enemies = FindObjectsOfType<EnemyAI>()
+            .Where(ai => ai.GetComponentInChildren<ScanNodeProperties>() is not null) // do not show what we cannot scan
+            .GroupBy(ai => ai.enemyType.enemyName)
+            .OrderBy(g => g.Key)
+            .ToList();
+
+        if (enemies.Count == 0)
+            return DefaultString;
+
+        StringBuilder sb = new();
+        foreach (var group in enemies)
+        {
+            sb.Append(group.Key);
+            sb.Append(": ");
+            sb.AppendLine(group.Count().ToString());
+        }
+        
+        sb.AppendLine();
+        return sb.ToString();
+    }
+}
